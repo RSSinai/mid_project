58e0bc1589ed2e560defe0e9a1bfd1d2efb6ff8d
Rony Sinai <rony.sinai@gmail.com>
2024-01-12 00:46:33 +0200

    check gps

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,73 @@
-import './App.css';
-import React, { useEffect, useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import "./App.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import L from "leaflet";
+import ButtonAppBar from "./components/navbar/Navbar";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-import icon from "./constants";
+function App() {
+  const [location, setLocation] = useState(null);
 
-export default function App() {
-  function LocationMarker() {
-    const [position, setPosition] = useState(null);
-    const [bbox, setBbox] = useState([]);
+  useEffect(() => {
+    const watchId = navigator.geolocation.watchPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setLocation({ latitude, longitude });
 
-    const map = useMap();
-
-    useEffect(() => {
-      map.locate().on("locationfound", function (e) {
-        setPosition(e.latlng);
-        map.flyTo(e.latlng, map.getZoom());
-        const radius = e.accuracy;
-        const circle = L.circle(e.latlng, radius);
-        circle.addTo(map);
-        setBbox(e.bounds.toBBoxString().split(","));
-      });
-    }, [map]);
-
-    return position === null ? null : (
-      <Marker position={position} icon={icon}>
-        <Popup>
-          You are here. <br />
-          Map bbox: <br />
-          <b>Southwest lng</b>: {bbox[0]} <br />
-          <b>Southwest lat</b>: {bbox[1]} <br />
-          <b>Northeast lng</b>: {bbox[2]} <br />
-          <b>Northeast lat</b>: {bbox[3]}
-        </Popup>
-      </Marker>
+        // You can send the location data to your server or perform any other actions here.
+        // Example: Sending data to a server using Axios
+        axios
+          .post("https://65a06c86600f49256faff5ec.mockapi.io/coordinates", { latitude, longitude })
+          .then((response) => {
+            console.log("Location data sent successfully:", response.data);
+          })
+          .catch((error) => {
+            console.error("Error sending location data:", error);
+          });
+      },
+      (error) => {
+        console.error("Error getting location:", error);
+      }
     );
-  }
+
+    // Cleanup function to clear the watch when the component unmounts
+    return () => {
+      navigator.geolocation.clearWatch(watchId);
+    };
+  }, []);
 
   return (
-    <MapContainer
-      center={[49.1951, 16.6068]}
-      zoom={13}
-      scrollWheelZoom
-      style={{ height: "100vh" }}
-    >
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <LocationMarker />
-    </MapContainer>
+    <>
+      <div>
+        <h1>Real-Time GPS Tracking</h1>
+        {location && (
+          <p>
+            Latitude: {location.latitude}, Longitude: {location.longitude}
+          </p>
+        )}
+      </div>
+      {/* <ButtonAppBar />
+      <MapContainer
+        center={[32.07953127200945, 34.76931791534278]}
+        zoom={18}
+        scrollWheelZoom
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[32.06812727294921, 34.763810682209794]}>
+          <Popup>Location 1: Graffiti Art</Popup>
+        </Marker>
+        <Marker position={[32.068944247417505, 34.76781237229591]}>
+          <Popup>Location 2: Graffiti Art</Popup>
+        </Marker>
+        <Marker position={[32.08605130872843, 34.768870639542655]}>
+          <Popup>Location 3: Graffiti Art</Popup>
+        </Marker>
+      </MapContainer> */}
+    </>
   );
 }
+
+export default App;
