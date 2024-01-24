fecf22a7a7946cb668046ca2ad6133b6e1e9c5a9
Rony Sinai <rony.sinai@gmail.com>
2024-01-24 04:06:06 +0200

    adding

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {  Route, Routes } from 'react-router-dom';
 import Map from "./pages/map/Map";
 import Main from "./pages/main/Main";
 import Login from "./pages/login/Login";
@@ -7,13 +7,13 @@ import Login from "./pages/login/Login";
 function App() {
   return (
     <>
-      <Router>
+      
         <Routes>
           <Route path="/" element={<Main/>}/>
           <Route path="/map" element={<Map/>}/>
           <Route path="/login" element={<Login/>}/>
         </Routes>
-      </Router>
+      
     </>
   );
 }
