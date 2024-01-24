f8cf988f56c81596b10ac2f642d66d6d45205fab
Rony Sinai <rony.sinai@gmail.com>
2024-01-24 14:52:12 +0200

    logo

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import './Hero.css'
+import React from "react";
+import "./Hero.css";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
-    return (
-        <div className="hero-section">
-          <div className="hero-content">
-            <h1>Walka.pro</h1>
-            <h2>Walk A PRO - Best professionals tours</h2>
-            <button class="button">LOG IN</button>
-          </div>
-        </div>
-      );
-}
+  return (
+    <div className="hero-section">
+      <div className="hero-content">
+        <h1>Walka.pro</h1>
+        <h2>Walk A PRO - Best tours built by professionals</h2>
+        <Link to="/login">
+          <button class="button">Login</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
