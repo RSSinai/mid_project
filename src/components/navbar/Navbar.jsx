432c497f00e08c422d278916c9f6f0b3a3dc690e
Rony Sinai <rony.sinai@gmail.com>
2024-01-24 15:02:17 +0200

    logo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -68,7 +68,7 @@ function ResponsiveAppBar() {
             variant="h6"
             noWrap
             component="a"
-            href="/main"
+            href="/"
             sx={{
               mr: 2,
               display: { xs: "flex", md: "none" },
