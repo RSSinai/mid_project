5bcd18c76b07565cc50319ae070970dd3613aac7
Rony Sinai <rony.sinai@gmail.com>
2024-01-24 04:42:27 +0200

    adding thisss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import {  Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Map from "./pages/map/Map";
 import Main from "./pages/main/Main";
 import Login from "./pages/login/Login";
@@ -7,16 +7,15 @@ import Login from "./pages/login/Login";
 function App() {
   return (
     <>
-      
+      <Router>
         <Routes>
-          <Route path="/" exact element={<Main/>}/>
-          <Route path="/map" element={<Map/>}/>
-          <Route path="/login" element={<Login/>}/>
+          <Route path="/" exact element={<Main />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
-      
+      </Router>
     </>
   );
 }
 
 export default App;
-
